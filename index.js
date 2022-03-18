87faa631d30faefa7db57b82c7f818b35aed709a
diegosantosouza <diegosantos.s@hotmail.com>
2022-03-18 00:46:20 -0300

    feat: get route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const axios = require('axios')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const { TOKEN, SERVER_URL } = process.env
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
@@ -10,12 +11,16 @@ const WEBHOOK_URL = SERVER_URL + URI
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 const init = async () => {
     const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`)
     console.log(res.data)
 }
 
+app.get('/', async (req, res) => {
+    res.send('Hello World!')
+})
 app.post(URI, async (req, res) => {
     console.log(req.body)
 
